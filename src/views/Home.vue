12746a16eff6a248d4d5ca673e5ca724a933d308
alexanderstueben <alexander.stueben@md.de>
2022-01-12 17:56:06 +0100

    Added Router and data fetching

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home"> 
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus id corrupti veniam officia delectus vel magnam laudantium facere pariatur blanditiis. Sunt porro mollitia architecto aperiam nihil sequi modi, exercitationem reprehenderit illo? Veritatis voluptate assumenda labore quidem? Nemo rerum doloribus ex deserunt commodi tenetur, unde saepe, impedit reprehenderit molestias illo minima quibusdam rem. In cumque debitis, dolore iusto, perferendis eos magni voluptatem facilis hic quaerat minima quia id magnam esse pariatur error quo aliquam? Officiis nisi sunt voluptate, vero asperiores unde facere cum nihil, nemo minus ducimus est. Cupiditate voluptas nulla nihil provident voluptates quos vel facere. Aspernatur ducimus a aliquam.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus id corrupti veniam officia delectus vel magnam laudantium facere pariatur blanditiis. Sunt porro mollitia architecto aperiam nihil sequi modi, exercitationem reprehenderit illo? Veritatis voluptate assumenda labore quidem? Nemo rerum doloribus ex deserunt commodi tenetur, unde saepe, impedit reprehenderit molestias illo minima quibusdam rem. In cumque debitis, dolore iusto, perferendis eos magni voluptatem facilis hic quaerat minima quia id magnam esse pariatur error quo aliquam? Officiis nisi sunt voluptate, vero asperiores unde facere cum nihil, nemo minus ducimus est. Cupiditate voluptas nulla nihil provident voluptates quos vel facere. Aspernatur ducimus a aliquam.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
